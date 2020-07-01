6b7df9e6d2b5897a19546869e90dc862f4e3122a
Alejandro Vivas <alejandrojv17@gmail.com>
2020-07-01 10:49:37 -0400

    Account recovery

--- src/pages/auth/RestoreAccont.js
+++ src/pages/auth/RestoreAccont.js
@@ -55,69 +55,69 @@ const RestoreAccont = () => {
     return ( 
         <Layout>
 
-<section className="section section-shaped section-lg">
-    <div className="shape shape-style-1 bg-gradient-default">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div className="container pt-lg-7">
-      <div className="row justify-content-center">
-        <div className="col-lg-5">
-          <div className="card bg-secondary shadow border-0">
-            <div className="card-body px-lg-5 py-lg-5">
-              <div className="text-center text-muted mb-4">
-                <small>Please, enter your new password.</small>
+          <section className="section section-shaped section-lg">
+            <div className="shape shape-style-1 bg-gradient-default">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div className="container pt-lg-7">
+              <div className="row justify-content-center">
+                <div className="col-lg-5">
+                  <div className="card bg-secondary shadow border-0">
+                    <div className="card-body px-lg-5 py-lg-5">
+                      <div className="text-center text-muted mb-4">
+                        <small>Please, enter your new password.</small>
+                      </div>
+                      <form onSubmit={handleSubmit}>
+                        <div className="form-group mb-3">
+                          <div className="input-group input-group-alternative">
+                            <div className="input-group-prepend">
+                            <span className="input-group-text"><i className="ni ni-lock-circle-open"></i></span>
+                            </div>
+                            <input className="form-control" type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value) } disabled={loading}/>
+                          </div>
+                        </div>
+                        <div className="form-group mb-3">
+                          <div className="input-group input-group-alternative">
+                            <div className="input-group-prepend">
+                            <span className="input-group-text"><i className="ni ni-lock-circle-open"></i></span>
+                            </div>
+                            <input className="form-control" type="password" name="confirmPassword" placeholder="Confirm Password" onChange={(e) => setConfirmPassword(e.target.value) } disabled={loading}/>
+                          </div>
+                        </div>
+                        {
+                            success && <div className="alert text-success p1">Done! Your password has been updated.</div>
+                        }
+                        {
+                            error && <div className="alert text-danger p1">{error}</div>
+                        }
+                        {
+                            invalidPayload && <div className="alert text-danger p1">{invalidPayload}</div>
+                        }
+                        <div className="text-center">
+                          <button disabled={loading} type="submit" className="btn btn-primary my-4">Request Link</button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                  <div className="row mt-3">
+                    <div className="col-6">
+                      <Link to="/login" className="text-light"><small>Login</small></Link>
+                    </div>
+                    <div className="col-6 text-right">
+                      <Link to="/" className="text-light"><small>Home</small></Link>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <form onSubmit={handleSubmit}>
-                <div className="form-group mb-3">
-                  <div className="input-group input-group-alternative">
-                    <div className="input-group-prepend">
-                    <span className="input-group-text"><i className="ni ni-lock-circle-open"></i></span>
-                    </div>
-                    <input className="form-control" type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value) } disabled={loading}/>
-                  </div>
-                </div>
-                <div className="form-group mb-3">
-                  <div className="input-group input-group-alternative">
-                    <div className="input-group-prepend">
-                    <span className="input-group-text"><i className="ni ni-lock-circle-open"></i></span>
-                    </div>
-                    <input className="form-control" type="password" name="confirmPassword" placeholder="Confirm Password" onChange={(e) => setConfirmPassword(e.target.value) } disabled={loading}/>
-                  </div>
-                </div>
-                {
-                    success && <div className="alert text-success p1">Done! Your password has been updated.</div>
-                }
-                {
-                    error && <div className="alert text-danger p1">{error}</div>
-                }
-                {
-                    invalidPayload && <div className="alert text-danger p1">{invalidPayload}</div>
-                }
-                <div className="text-center">
-                  <button disabled={loading} type="submit" className="btn btn-primary my-4">Request Link</button>
-                </div>
-              </form>
             </div>
-          </div>
-          <div className="row mt-3">
-            <div className="col-6">
-              <Link to="/login" className="text-light"><small>Login</small></Link>
-            </div>
-            <div className="col-6 text-right">
-              <Link to="/" className="text-light"><small>Home</small></Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+          </section>
         </Layout>
      );
 }
